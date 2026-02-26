9fb8bda39e03070bd99fb6390641559e1bd9a074
Krist Ponpairin <krist7599555@gmail.com>
2026-02-26 16:25:46 +0700

    feat: add seo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,37 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { MetaTags } from 'svelte-meta-tags';
 
 	let { children } = $props();
+	const title = 'Toyota Visualization';
+	const desc = 'Fun and interactive Visualization for multiple grading score';
+	const og = 'https://krist7599555.github.io/toyota-visualize/screenshot.jpg';
+	const url = 'https://krist7599555.github.io/toyota-visualize';
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<MetaTags
+	{title}
+	titleTemplate="%s | {title}"
+	description={desc}
+	openGraph={{
+		url: url,
+		title,
+		description: desc,
+		images: [{ url: og, width: 2642, height: 1748, alt: title }],
+		siteName: title
+	}}
+	twitter={{
+		site: '@site',
+		cardType: 'summary_large_image',
+		title: title,
+		description: desc,
+		image: og,
+		imageAlt: title
+	}}
+/>
 {@render children()}
